0d087187bc1454243359e96e43e84283e1bc8b20
江唯一 <86941637+Zhoujinnian@users.noreply.github.com>
2023-06-01 23:22:07 +0800

    Add files via upload

--- smart_ptr/unique_ptr.cpp
+++ smart_ptr/unique_ptr.cpp
@@ -11,6 +11,10 @@ public:
 		this->a = a;
 		this->b = b;
 	}
+	~A()
+	{
+		cout << "½áÊø" << endl;
+	}
 	int show()
 	{
 		return a+b;
@@ -19,8 +23,14 @@ public:
 
 int main()
 {
+	//unique_ptr ¶¨Òå£º
+	
 	//unique_ptr<A> p1(new A(5,6));
-	unique_ptr<A>p1 = make_unique<A>(5,6);
-	cout << p1->show()<< endl;
+	unique_ptr<A>p1 = make_unique<A>(5, 6);
+	//unique_ptr<A>p2 = make_unique<A>(5,0);
 
+	cout << p1->show() << endl;
+	//p2 = move(p1);
+	////cout << p1->show() << endl;
+	//cout << p2->show() << endl;
 }
